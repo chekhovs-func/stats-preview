83a38da8f4af16257acb18bb16f2a03b4716b5f7
Robert Butler <robertbutler@protonmail.com>
2022-03-02 20:32:27 +0000

    feat: apply responsive styles for desktop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,9 +6,14 @@ export default function Card() {
       <img
         src='assets/image-header-mobile.png'
         alt='Colleagues sitting around a desk'
-        className='card-hero-img'
+        className='card-hero-img-mob'
       />
-      <section className='card-txt-wrap'>
+      <img
+        src='assets/image-header-desktop.png'
+        alt='Colleagues sitting around a desk'
+        className='card-hero-img-dsk'
+      />
+      <article className='card-txt-wrap'>
         <h1 className='card-hdn'>
           Get <span className='card-hdg-acn'>insights</span> that help your business grow.
         </h1>
@@ -16,15 +21,21 @@ export default function Card() {
           Discover the benefits of data analytics and make better decisions regarding
           revenue, customer experience, and overall efficiency.
         </p>
-      </section>
-      <section className='card-sta-wrap'>
-        <h2 className='card-sta-hdn'>10k+</h2>
-        <h3 className='card-sta-sub'>COMPANIES</h3>
-        <h2 className='card-sta-hdn'>314</h2>
-        <h3 className='card-sta-sub'>TEMPLATES</h3>
-        <h2 className='card-sta-hdn'>12M+</h2>
-        <h3 className='card-sta-sub'>QUERIES</h3>
-      </section>
+        <article className='card-sta-wrap'>
+          <aside className='card-sta'>
+            <h2 className='card-sta-hdn'>10k+</h2>
+            <h3 className='card-sta-sub'>COMPANIES</h3>
+          </aside>
+          <aside className='card-sta'>
+            <h2 className='card-sta-hdn'>314</h2>
+            <h3 className='card-sta-sub'>TEMPLATES</h3>
+          </aside>
+          <aside className='card-sta'>
+            <h2 className='card-sta-hdn'>12M+</h2>
+            <h3 className='card-sta-sub'>QUERIES</h3>
+          </aside>
+        </article>
+      </article>
     </section>
   );
 }
